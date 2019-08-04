4dd33b1bd8294b4e69e44e000ed93b7ab390fd6e
Lukasz Ostrowski <lukasz@ostrowski.com.pl>
2019-08-04 19:53:33 +0200

    feat(ie): adds polyfill for Fetch API

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/**
+ * Polyfill for IE10
+ * Normally should be feature-detected for performance reasons
+ */
+import 'whatwg-fetch';
+
 import App from './app/app';
 import { StoreProvider } from './app/store/store-provider';
 import './index.module.scss';
